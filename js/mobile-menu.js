71af71a8b4a0618620bdc0b6b4771be6e85801da
BorschVS <borschvadym@gmail.com>
2023-07-14 17:04:38 +0300

    Виконав фікси модального вікна та мобільного меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -11,4 +11,20 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+
+  (() => {
+    const refs = {
+      openMobileBtn: document.querySelector("[data-mobile-open]"),
+      closeMobileBtn: document.querySelector("[data-mobile-close]"),
+      mobile: document.querySelector("[data-mobile]"),
+    };
+  
+    refs.openMobileBtn.addEventListener("click", toggleMobile);
+    refs.closeMobileBtn.addEventListener("click", toggleMobile);
+  
+    function toggleMobile() {
+      document.body.classList.toggle("mobile-open");
+      refs.mobile.classList.toggle("is-hidden");
+    }
+  })();
 })();
